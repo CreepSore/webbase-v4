b95a809ab944a9c7022ebea28acbc315170aa5b7
Eric <eric.himmelbauer@protonmail.com>
2022-09-21 22:26:48 +0200

    added multi-protocol support for nexus-server

--- extensions/Core.Databridge/protocols/DatabridgeNexusServer.ts
+++ extensions/Core.Databridge/protocols/DatabridgeNexusServer.ts
@@ -26,66 +26,79 @@ class NexusChannel {
 }
 
 export default class DatabridgeNexusServer {
-    protocol: IDatabridgeServerProtocol;
+    protocols: IDatabridgeServerProtocol[];
     channels: NexusChannel[];
 
-    constructor(protocol: IDatabridgeServerProtocol) {
-        this.protocol = protocol;
+    constructor() {
+        this.protocols = [];
         this.channels = [];
     }
 
     async start() {
-        this.protocol.onClientConnected(client => {
-            let clientId = uuid.v4();
-            console.log("INFO", "NexusServer", `Client with id ${clientId} connected.`);
-
-            client.sendPacket(new DatabridgePacket("HANDSHAKE", {
-                clientId
-            }, {}));
-
-            client.onPacketReceived(packet => {
-                if(packet.type === "SUBSCRIBE") {
-                    let subPacket = packet as IDatabridgePacket<{channelName: string, secret?: string}>;
-                    let {channelName, secret} = subPacket.data;
-                    let channel = this.channels.find(channel => channel.name === channelName);
-                    if(!channel) {
-                        channel = new NexusChannel();
-                        channel.name = channelName;
-                        channel.secret = secret || null;
-                        this.channels.push(channel);
-                    }
-
-                    if((!channel.secret || channel.secret === secret) && !channel.clients.includes(client)) {
-                        console.log("INFO", "NexusServer", `Client ${clientId} subscribed to ${channelName}.`);
-                        channel.subscribeClient(client);
-                        channel.broadcast(new DatabridgePacket("JOINED", {clientId, channel: channel.name}, {}));
-                    }
-                }
-                else if(packet.type === "UNSUBSCRIBE") {
-                    let unsubPacket = packet as IDatabridgePacket<{channelName: string}>;
-                    let channel = this.channels.find(channel => channel.name === unsubPacket.data.channelName);
-                    if(!channel || !channel.clients.includes(client)) return;
-
-                    channel.broadcast(new DatabridgePacket("LEAVED", {clientId, channel: channel.name}, {}));
-                    channel.unsubscribeClient(client);
-                    console.log("INFO", "NexusServer", `Client ${clientId} unsubscribed from ${unsubPacket.data.channelName}.`);
-                }
-                else if(packet.type === "BROADCAST") {
-                    let bcPacket = packet as IDatabridgePacket<{channelName: string, clientId: string}>;
-                    let channel = this.channels.find(channel => channel.name === bcPacket.data.channelName);
-                    if(!channel || !channel.clients.includes(client)) return;
-                    bcPacket.data.clientId = clientId;
-
-                    channel.broadcast(bcPacket, c => c !== client);
-                    console.log("INFO", "NexusServer", `Client ${clientId} broadcasted to ${channel.clients.length - 1} clients.`);
-                }
+        await Promise.all(this.protocols.map(async protocol => {
+            protocol.onClientConnected(client => {
+                this.onClientConnected(client);
             });
-        });
 
-        await this.protocol.start();
+            await protocol.start();
+        }));
     }
 
     async stop() {
-        await this.protocol.stop();
+        await Promise.all(this.protocols.map(async protocol => {
+            await protocol.stop();
+        }));
+    }
+
+    addProtocol(protocol: IDatabridgeServerProtocol) {
+        this.protocols.push(protocol);
+        return this;
+    }
+
+    onClientConnected(client: IDatabridgeSocket) {
+        let clientId = uuid.v4();
+        console.log("INFO", "NexusServer", `Client with id ${clientId} connected.`);
+
+        client.sendPacket(new DatabridgePacket("HANDSHAKE", {
+            clientId
+        }, {}));
+
+        client.onPacketReceived(packet => {
+            if(packet.type === "SUBSCRIBE") {
+                let subPacket = packet as IDatabridgePacket<{channelName: string, secret?: string}>;
+                let {channelName, secret} = subPacket.data;
+                let channel = this.channels.find(channel => channel.name === channelName);
+                if(!channel) {
+                    channel = new NexusChannel();
+                    channel.name = channelName;
+                    channel.secret = secret || null;
+                    this.channels.push(channel);
+                }
+
+                if((!channel.secret || channel.secret === secret) && !channel.clients.includes(client)) {
+                    console.log("INFO", "NexusServer", `Client ${clientId} subscribed to ${channelName}.`);
+                    channel.subscribeClient(client);
+                    channel.broadcast(new DatabridgePacket("JOINED", {clientId, channel: channel.name}, {}));
+                }
+            }
+            else if(packet.type === "UNSUBSCRIBE") {
+                let unsubPacket = packet as IDatabridgePacket<{channelName: string}>;
+                let channel = this.channels.find(channel => channel.name === unsubPacket.data.channelName);
+                if(!channel || !channel.clients.includes(client)) return;
+
+                channel.broadcast(new DatabridgePacket("LEAVED", {clientId, channel: channel.name}, {}));
+                channel.unsubscribeClient(client);
+                console.log("INFO", "NexusServer", `Client ${clientId} unsubscribed from ${unsubPacket.data.channelName}.`);
+            }
+            else if(packet.type === "BROADCAST") {
+                let bcPacket = packet as IDatabridgePacket<{channelName: string, clientId: string}>;
+                let channel = this.channels.find(channel => channel.name === bcPacket.data.channelName);
+                if(!channel || !channel.clients.includes(client)) return;
+                bcPacket.data.clientId = clientId;
+
+                channel.broadcast(bcPacket, c => c !== client);
+                console.log("INFO", "NexusServer", `Client ${clientId} broadcasted to ${channel.clients.length - 1} clients.`);
+            }
+        });
     }
 }
