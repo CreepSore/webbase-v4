cf3bb7a4a68e60fe2c248dd26aced9174ed8a887
Eric <eric.himmelbauer@protonmail.com>
2022-11-01 11:58:05 +0100

    Added Core.Cache

---
+++ extensions/Core.Cache/index.ts
@@ -0,0 +1,137 @@
+import * as util from "util";
+import {EventEmitter} from "events";
+
+import IExecutionContext from "@service/extensions/IExecutionContext";
+import IExtension, { ExtensionMetadata } from "@service/extensions/IExtension";
+import ConfigLoader from "@logic/config/ConfigLoader";
+
+class CacheConfig {
+
+}
+
+interface CacheEntryConfig<T> {
+    key: string;
+    updateCallback: () => Promise<T> | T;
+    defaultValue?: T;
+    updateEveryMs?: number;
+}
+
+class CacheEntry<T> {
+    key: string;
+    currentValue: T;
+    defaultValue?: T;
+    refreshNextUpdate?: boolean;
+    lastUpdate: Date;
+    updateEveryMs?: number;
+    private updateCallback: () => Promise<T> | T;
+
+    constructor(config: CacheEntryConfig<T>) {
+        this.key = config.key;
+        this.updateCallback = config.updateCallback;
+        this.currentValue = this.defaultValue = config.defaultValue;
+        this.lastUpdate = this.defaultValue ? new Date() : new Date(0);
+        this.updateEveryMs = config.updateEveryMs;
+        this.refreshNextUpdate = !this.defaultValue;
+    }
+
+    async getValue() {
+        if(this.refreshNextUpdate || (this.updateEveryMs > 0 && (Date.now() - Number(this.lastUpdate) > this.updateEveryMs))) {
+            console.log("INFO", "Core.Cache", `Executing update-function for cache [${this.key}]`);
+            this.currentValue = await this.updateCallback();
+            console.log("INFO", "Core.Cache", `Updated value for cache [${this.key}] to [${util.inspect(this.currentValue, {breakLength: Infinity})}]`);
+        }
+
+        return this.currentValue;
+    }
+
+    invalidate(updateNow = false): void | Promise<T> {
+        this.refreshNextUpdate = true;
+        if(updateNow) {
+            return this.getValue();
+        }
+        console.log("INFO", "Core.Cache", `Invalidated cache [${this.key}]`);
+    }
+}
+
+export default class CoreCache implements IExtension {
+    metadata: ExtensionMetadata = {
+        name: "Core.Cache",
+        version: "1.0.0",
+        description: "Caching Module",
+        author: "ehdes",
+        dependencies: ["Core"]
+    };
+
+    config: CacheConfig;
+    configLoader: ConfigLoader<typeof this.config>;
+    events: EventEmitter = new EventEmitter();
+    cache: Map<string, CacheEntry<any>> = new Map();
+
+    constructor() {
+        this.config = this.loadConfig();
+    }
+
+    async start(executionContext: IExecutionContext) {
+        this.checkConfig();
+        if(executionContext.contextType === "cli") {
+            return;
+        }
+    }
+
+    async stop() {
+        
+    }
+
+    createCacheEntry<T>(config: CacheEntryConfig<T>) {
+        let entry = new CacheEntry<T>(config);
+        this.cache.set(entry.key, entry);
+        return entry;
+    }
+
+    cacheEntryExists(key: string) {
+        return this.cache.has(key);
+    }
+
+    getCacheEntry<T>(key: string) {
+        return this.cache.get(key) as CacheEntry<T>;
+    }
+
+    getCachedValue<T>(key: string, defaultValue: T) {
+        return (this.getCacheEntry(key)?.currentValue || defaultValue) as T;
+    }
+
+    invalidateCache(key: string, updateNow = false) {
+        let entry = this.cache.get(key);
+        if(entry) {
+            entry.invalidate(updateNow);
+        }
+    }
+
+    clearCache() {
+        this.cache.clear();
+    }
+
+    private checkConfig() {
+        if(!this.config) {
+            throw new Error(`Config could not be found at [${this.configLoader.configPath}]`);
+        }
+    }
+
+    private loadConfig() {
+        let model = new CacheConfig();
+        if(Object.keys(model).length === 0) return model;
+
+        let [cfgname, templatename] = this.generateConfigNames();
+        this.configLoader = new ConfigLoader(cfgname, templatename);
+        let cfg = this.configLoader.createTemplateAndImport(model);
+
+        return cfg;
+    }
+
+    private generateConfigNames() {
+        return [
+            ConfigLoader.createConfigPath(`${this.metadata.name}.json`),
+            ConfigLoader.createConfigPath(`${this.metadata.name}.template.json`)
+        ];
+    }
+}
