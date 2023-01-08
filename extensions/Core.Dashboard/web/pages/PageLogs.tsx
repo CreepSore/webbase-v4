23ff5a1a0833bdade151892cbb7913488ce64489
Eric <eric.himmelbauer@protonmail.com>
2023-01-08 18:46:20 +0100

    fixed log tooltip

--- extensions/Core.Dashboard/web/pages/PageLogs.tsx
+++ extensions/Core.Dashboard/web/pages/PageLogs.tsx
@@ -96,7 +96,7 @@ function LogViewer(props: LogViewerProperties) {
                         value={search1}
                         onChange={e => setSearch1(e.target.value)}
                         placeholder="Date"
-                        title="Prefix search with '?' to enable regex or '?' to negate search"
+                        title="Prefix search with '?' to enable regex or '!' to negate search"
                     /></th>
                     <th><input
                         className="w-full px-2 py-1"
@@ -104,7 +104,7 @@ function LogViewer(props: LogViewerProperties) {
                         value={search2}
                         onChange={e => setSearch2(e.target.value)}
                         placeholder="Level"
-                        title="Prefix search with '?' to enable regex or '?' to negate search"
+                        title="Prefix search with '?' to enable regex or '!' to negate search"
                     /></th>
                     <th><input
                         className="w-full px-2 py-1"
@@ -112,7 +112,7 @@ function LogViewer(props: LogViewerProperties) {
                         value={search3}
                         onChange={e => setSearch3(e.target.value)}
                         placeholder="Segments"
-                        title="Prefix search with '?' to enable regex or '?' to negate search"
+                        title="Prefix search with '?' to enable regex or '!' to negate search"
                     /></th>
                     <th><input
                         className="w-full px-2 py-1"
@@ -120,7 +120,7 @@ function LogViewer(props: LogViewerProperties) {
                         value={search4}
                         onChange={e => setSearch4(e.target.value)}
                         placeholder="Message"
-                        title="Prefix search with '?' to enable regex or '?' to negate search"
+                        title="Prefix search with '?' to enable regex or '!' to negate search"
                     /></th>
                     <th></th>
                 </tr>
