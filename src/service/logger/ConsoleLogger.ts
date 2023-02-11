47b30e23d2b02a7fcb3c147382ace26b6e867a41
Eric <eric.himmelbauer@protonmail.com>
2023-02-11 23:19:43 +0100

    Fixed logger formatting

--- src/service/logger/ConsoleLogger.ts
+++ src/service/logger/ConsoleLogger.ts
@@ -6,7 +6,7 @@ export default class ConsoleLogger implements ILogger {
         let infos = [...args].slice(0, args.length - 1);
 
         let message: string = args[args.length - 1];
-        let formatted = `[${new Date().toISOString()}][${level.toUpperCase().padStart(8, " ")}]${infos.map(i => `[${i}]`)} ${message}`;
+        let formatted = `[${new Date().toISOString()}][${level.toUpperCase().padStart(8, " ")}]${infos.map(i => `[${i}]`).join("")} ${message}`;
         if(Boolean(LoggerService.oldLog)) {
             LoggerService.oldLog(formatted);
         }
