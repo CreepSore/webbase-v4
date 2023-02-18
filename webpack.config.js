56327a9fdacedff7f86753ad975926405f112e19
Eric <eric.himmelbauer@protonmail.com>
2023-02-18 16:13:06 +0100

    made webpack config more dynamic

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,157 @@ let path = require("path");
 let fs = require("fs");
 let {merge: webpackMerge} = require("webpack-merge");
 
+let extPath = path.join(__dirname, "extensions");
+
+let extensionConfigs = fs.readdirSync(extPath)
+    .filter(file => !file.startsWith("Custom.Template") && file.endsWith("webpack.json"))
+    .map(file => {
+        let filePath = path.join(extPath, file);
+        let data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+        return data || {};
+    });
+
+const buildAppConfig = (env, argv) => {
+    extensionConfigs = extensionConfigs.map(cfg => cfg.appBuildConfig).filter(Boolean);
+
+    return webpackMerge([{
+        entry: {
+            "app.js": path.resolve(__dirname, "src", "app.ts")
+        },
+        output: {
+            path: path.resolve(__dirname, "out"),
+            filename: "[name].js",
+            clean: true
+        },
+        devtool: argv.mode === "development" ? "inline-source-map" : false,
+        resolve: {
+            extensions: [".ts", ".js", ".json"],
+            alias: {
+                wpextensions: path.resolve(__dirname, "extensions"),
+                "@extensions": path.resolve(__dirname, "extensions")
+            },
+            modules: ['node_modules']
+        },
+        externals: {},
+        module: {
+            rules: [
+                {
+                    oneOf: [
+                        {
+                            test: /\.(ts|js)$/i,
+                            exclude: /(node_modules)/,
+                            loader: "babel-loader",
+                            options: {
+                                plugins: [
+                                    ["babel-plugin-tsconfig-paths", {
+                                        rootDir: ".",
+                                        tsconfig: "tsconfig.json",
+                                    }]
+                                ],
+                                presets: ["@babel/typescript", ["@babel/preset-env", {
+                                    targets: {
+                                        node: "17"
+                                    }
+                                }]]
+                            },
+                        },
+                        {
+                            test: /\.(tsx|jsx)$/i,
+                            exclude: /(node_modules)/,
+                            use: [
+                                {
+                                    loader: "babel-loader",
+                                    options: {
+                                        presets: ["@babel/typescript", "@babel/preset-react", ["@babel/preset-env", {
+                                            targets: {
+                                                node: "17"
+                                            }
+                                        }]]
+                                    }
+                                }
+                            ],
+                            type: "asset/resource"
+                        },
+                        {
+                            test: /\.(json)$/i,
+                            exclude: /(node_modules)/
+                        },
+                        {
+                            test: /\.png$/i,
+                            type: "asset/inline"
+                        },
+                        { test: /\.(css)$/i, use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"] }
+                    ]
+                }
+            ]
+        },
+        optimization: {
+            minimize: argv.mode === "productive",
+            mangleExports: false
+        },
+        plugins: [ ],
+        target: "node"
+    }, ...extensionConfigs]);
+};
+
+const buildWebConfig = (env, argv) => {
+    extensionConfigs = extensionConfigs.map(cfg => cfg.webBuildConfig).filter(Boolean);
+
+    return webpackMerge([{
+        entry: {},
+        output: {
+            path: path.resolve(__dirname, "out"),
+            filename: "[name]"
+        },
+        devtool: argv.mode === "development" ? "inline-source-map" : false,
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".jsx"],
+            modules: ['node_modules'],
+            alias: {
+                "@extensions": extPath
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)$/i,
+                    exclude: /(node_modules)/,
+                    loader: "babel-loader",
+                    options: {
+                        plugins: [],
+                        presets: [["@babel/preset-env", {
+                            targets: "> 0.25%, not dead"
+                        }], "@babel/typescript"]
+                    },
+                },
+                {
+                    test: /\.(tsx|jsx)$/i,
+                    exclude: /(node_modules)/,
+                    use: [
+                        {
+                            loader: "babel-loader",
+                            options: {
+                                presets: ["@babel/typescript", "@babel/preset-react", "@babel/preset-env"]
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.png$/i,
+                    type: "asset/inline"
+                },
+                { test: /\.(css)$/i, exclude: /(node_modules)/, use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"] }
+            ]
+        },
+        optimization: {
+            minimize: argv.mode === "productive",
+            mangleExports: false,
+            chunkIds: "named"
+        },
+        plugins: [ ],
+    }, ...extensionConfigs]);
+}
+
 module.exports = function(env, argv) {
     let buildType;
     if(!env.buildtype) {
@@ -16,156 +167,10 @@ module.exports = function(env, argv) {
     }
 
     if(buildType === "app") {
-        return {
-            entry: {
-                "app.js": path.resolve(__dirname, "src", "app.ts")
-            },
-            output: {
-                path: path.resolve(__dirname, "out"),
-                filename: "[name].js",
-                clean: true
-            },
-            devtool: argv.mode === "development" ? "inline-source-map" : false,
-            resolve: {
-                extensions: [".ts", ".js", ".json"],
-                alias: {
-                    wpextensions: path.resolve(__dirname, "extensions"),
-                    "@extensions": path.resolve(__dirname, "extensions")
-                },
-                modules: ['node_modules']
-            },
-            externals: {
-                knex: "commonjs knex",
-                express: "commonjs express",
-                "express-ws": "commonjs express-ws",
-                "utf-8-validate": "commonjs utf-8-validate",
-                bufferutil: "commonjs bufferutil"
-            },
-            module: {
-                rules: [
-                    {
-                        oneOf: [
-                            {
-                                test: /\.(ts|js)$/i,
-                                exclude: /(node_modules)/,
-                                loader: "babel-loader",
-                                options: {
-                                    plugins: [
-                                        ["babel-plugin-tsconfig-paths", {
-                                            rootDir: ".",
-                                            tsconfig: "tsconfig.json",
-                                        }]
-                                    ],
-                                    presets: ["@babel/typescript", ["@babel/preset-env", {
-                                        targets: {
-                                            node: "17"
-                                        }
-                                    }]]
-                                },
-                            },
-                            {
-                                test: /\.(tsx|jsx)$/i,
-                                exclude: /(node_modules)/,
-                                use: [
-                                    {
-                                        loader: "babel-loader",
-                                        options: {
-                                            presets: ["@babel/typescript", "@babel/preset-react", ["@babel/preset-env", {
-                                                targets: {
-                                                    node: "17"
-                                                }
-                                            }]]
-                                        }
-                                    }
-                                ],
-                                type: "asset/resource"
-                            },
-                            {
-                                test: /\.(json)$/i,
-                                exclude: /(node_modules)/
-                            },
-                            {
-                                test: /\.png$/i,
-                                type: "asset/inline"
-                            },
-                            { test: /\.(css)$/i, use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"] }
-                        ]
-                    }
-                ]
-            },
-            optimization: {
-                minimize: argv.mode === "productive",
-                mangleExports: false
-            },
-            plugins: [ ],
-            target: "node"
-        }
+        return buildAppConfig(env, argv);
     }
     else if(buildType === "web") {
-        let extPath = path.join(__dirname, "extensions");
-
-        let cfg = webpackMerge([{
-            entry: {},
-            output: {
-                path: path.resolve(__dirname, "out"),
-                filename: "[name]"
-            },
-            devtool: argv.mode === "development" ? "inline-source-map" : false,
-            resolve: {
-                extensions: [".ts", ".tsx", ".js", ".jsx"],
-                modules: ['node_modules'],
-                alias: {
-                    "@extensions": extPath
-                }
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.(ts|js)$/i,
-                        exclude: /(node_modules)/,
-                        loader: "babel-loader",
-                        options: {
-                            plugins: [],
-                            presets: [["@babel/preset-env", {
-                                targets: "> 0.25%, not dead"
-                            }], "@babel/typescript"]
-                        },
-                    },
-                    {
-                        test: /\.(tsx|jsx)$/i,
-                        exclude: /(node_modules)/,
-                        use: [
-                            {
-                                loader: "babel-loader",
-                                options: {
-                                    presets: ["@babel/typescript", "@babel/preset-react", "@babel/preset-env"]
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        test: /\.png$/i,
-                        type: "asset/inline"
-                    },
-                    { test: /\.(css)$/i, exclude: /(node_modules)/, use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"] }
-                ]
-            },
-            optimization: {
-                minimize: argv.mode === "productive",
-                mangleExports: false,
-                chunkIds: "named"
-            },
-            plugins: [ ],
-        }, ...fs.readdirSync(extPath)
-            .filter(file => !file.startsWith("Custom.Template") && file.endsWith("webpack.json"))
-            .map(file => {
-                let filePath = path.join(extPath, file);
-                let data = JSON.parse(fs.readFileSync(filePath, "utf8"));
-                return data;
-            })
-        ]);
-
-        return cfg;
+        return buildWebConfig(env, argv);
     }
     else {
         return null;
