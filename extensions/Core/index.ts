ff997177d3698375a3d68fcc6285bbc10339e31b
Eric <eric.himmelbauer@protonmail.com>
2023-03-01 19:58:56 +0100

    added runPlatformDependent to Core

--- extensions/Core/index.ts
+++ extensions/Core/index.ts
@@ -65,4 +65,25 @@ export default class Core implements IExtension {
             }
         });
     }
+
+    runPlatformDependent<T>(callbacks: {
+        aix?: (platform: string) => T,
+        android?: (platform: string) => T,
+        darwin?: (platform: string) => T,
+        freebsd?: (platform: string) => T,
+        haiku?: (platform: string) => T,
+        linux?: (platform: string) => T,
+        openbsd?: (platform: string) => T,
+        sunos?: (platform: string) => T,
+        win32?: (platform: string) => T,
+        cygwin?: (platform: string) => T,
+        netbsd?: (platform: string) => T
+    }): T {
+        const callbackToRun = callbacks[process.platform];
+        if(!callbackToRun) {
+            throw new Error("Unsupported platform");
+        }
+
+        return callbackToRun(process.platform);
+    }
 }
