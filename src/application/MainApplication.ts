ce7a40c7fc0d173e823128a1cc6da7d30708116c
Eric <eric.himmelbauer@protonmail.com>
2023-12-21 20:09:21 +0100

    removed test code

--- src/application/MainApplication.ts
+++ src/application/MainApplication.ts
@@ -27,8 +27,6 @@ export default class MainAppliation implements IApplication {
 
         console.log("INFO", "MainApplication.ts", "Main Application Startup successful.");
         this.events.emit("after-startup", this.extensionService.executionContext);
-
-        ChildApplication.startChildApplication("test", 10000, "inherit");
     }
 
     async stop(): Promise<void> {
