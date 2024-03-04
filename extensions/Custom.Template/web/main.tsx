200a022b3621c655d0fb9b31b2f31f92a5b2c8d8
Eric <eric.himmelbauer@protonmail.com>
2024-03-04 16:11:54 +0100

    added web files to template

---
+++ extensions/Custom.Template/web/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./style.scss";
+import {createTheme} from "@mui/material/styles";
+import ThemeProvider from "@mui/system/ThemeProvider";
+
+function Main(): JSX.Element {
+    const theme = createTheme({
+        palette: {
+            mode: "dark",
+        },
+    });
+
+    return <ThemeProvider theme={theme}>
+
+    </ThemeProvider>;
+}
+
+window.addEventListener("load", () => {
+    document.title = "Template";
+    ReactDOM.createRoot(document.querySelector("#react-container") as Element).render(<Main />);
+});
+
