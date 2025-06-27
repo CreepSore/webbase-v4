e811b3da7d87298edb92e5b94e3884250be09b17
CreepSore <erichimmelbauer@gmail.com>
2025-06-27 15:56:58 +0200

    removed worker thread test

--- test/WorkerThread.test.ts
+++
@@ -1,9 +0,0 @@
-import WorkerThread from "../src/logic/threads/WorkerThread";
-
-describe("WorkerThread Tests", () => {
-    it("should create a worker thread", async() => {
-        const thread = new WorkerThread();
-        await thread.start();
-        await thread.waitForExit();
-    });
-});
