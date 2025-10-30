a85c02bfdf85c052726f581c434c72d8c9faa3e9
CreepSore <erichimmelbauer@gmail.com>
2025-10-30 19:38:09 +0100

    Added ObjectPool<T> because idk

---
+++ src/service/utils/ObjectPool.ts
@@ -0,0 +1,107 @@
+export type ObjectPoolOptions<T> = {
+    builder: () => Promise<T>;
+    destructor?: (instance: T) => Promise<void>;
+
+    max: number;
+    min?: number;
+}
+
+export default class ObjectPool<T> {
+    private _options: ObjectPoolOptions<T>;
+
+    private _isStarted: boolean;
+
+    private _free: Set<T> = new Set();
+    private _used: Set<T> = new Set();
+    private _waitingGets: Array<(instance: T) => void> = [];
+
+    get total() {
+        return this._free.size + this._used.size;
+    }
+
+    constructor(options: ObjectPoolOptions<T>) {
+        this._options = options;
+    }
+
+    async start() {
+        if(this._isStarted) {
+            return;
+        }
+
+        this._isStarted = true;
+
+        await this.ensureMinimum();
+    }
+
+    stop() {
+        if(!this._isStarted) {
+            return;
+        }
+
+        this._isStarted = false;
+
+        if(this._options.destructor) {
+            const toAwait = [];
+
+            for(const instance of [...this._used]) {
+                toAwait.push(this._options.destructor(instance));
+            }
+
+            for(const instance of [...this._free]) {
+                toAwait.push(this._options.destructor(instance));
+            }
+
+            return Promise.all(toAwait);
+        }
+
+        return Promise.resolve();
+    }
+
+    async get(): Promise<T> {
+        await this.ensureMinimum();
+
+        let thread: T = this._free.values().next().value;
+
+        if(!thread) {
+            if(this._free.size === 0 && this._used.size >= this._options.max) {
+                return new Promise<T>(res => {
+                    this._waitingGets.push((thread) => {
+                        res(thread);
+                    });
+                });
+            }
+
+            thread = await this._options.builder();
+        }
+
+        this._free.delete(thread);
+        this._used.add(thread);
+
+        return thread;
+    }
+
+    free(instance: T): this {
+        if(!this._used.has(instance)) {
+            return this;
+        }
+
+        if(this._waitingGets.length > 0) {
+            const instanceFreeCallback = this._waitingGets.shift();
+            if(instanceFreeCallback !== undefined) {
+                instanceFreeCallback(instance);
+                return;
+            }
+        }
+
+        this._used.delete(instance);
+        this._free.add(instance);
+        return this;
+    }
+
+    private async ensureMinimum(): Promise<void> {
+        const toCreate = this._options.min - this.total;
+        for(let i = 0; i < toCreate; i++) {
+            this._free.add(await this._options.builder());
+        }
+    }
+}
